62f82c4186443fd5a65096668d080f5056caf18d
j <93398891+basaaj@users.noreply.github.com>
2023-12-13 13:41:09 -0600

    Implement getting computer's choice

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    let choices = ['rock', 'paper', 'scissors'];
+    let choice = Math.floor(Math.random() * 3);
+    return choices[choice];
+}
+
+console.log(getComputerChoice());
