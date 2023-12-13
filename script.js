d82be244b1842165e3a06ba8e66d05c46f053723
j <93398891+basaaj@users.noreply.github.com>
2023-12-13 14:14:20 -0600

    Implement round of game where winner is determined

--- script.js
+++ script.js
@@ -4,4 +4,29 @@ function getComputerChoice() {
     return choices[choice];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let playerSelection = playerSelection.toLowerCase();
+    let win = 'You won!' + playerSelection + 'beats ' + computerSelection;
+    let lose = 'You lose!' + computerSelection + 'beats ' + playerSelection;
+    let result;
+
+    if (playerSelection == computerSelection) {
+        playRound(playerSelection, getComputerSelection());
+    }
+
+    else {
+        if (playerSelection == 'rock') {
+            result = computerSelection == 'scissors' ? win : lose;
+        }
+
+        else if (playerSelection == 'scissors') {
+            result = computerSelection == 'paper' ? win : lose;
+        }
+
+        else if (playerSelection == 'paper') {
+            result = computerSelection == 'rock' ? win : lose;
+        }
+    }
+
+    return result;
+}
